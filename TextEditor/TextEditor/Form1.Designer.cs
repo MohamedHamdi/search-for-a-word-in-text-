2ab2714c64177ebdf545375ef34de672ec1ec04f
MohamedHamdi <mohamed_hamdy_se@yahoo.com>
2017-05-15 08:29:17 -0700

    noComment

---
+++ TextEditor/TextEditor/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TextEditor
+{
+    partial class TEform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(599, 353);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 371);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(304, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 397);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 396);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(304, 20);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Replace";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(307, 423);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(304, 20);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reverse Selected";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // TEform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(623, 455);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "TEform";
+            this.Text = "TextEditor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TextEditor/TextEditor/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+
+    public partial class TEform : Form
+    {
+        StringBuilder myString;
+        string Flag;
+        byte[] Asciibytes;
+        byte[] Asciibytes2;
+        List<int> SpaceIndexes = new List<int>();
+
+
+        public TEform()
+        {
+            InitializeComponent();
+            myString = new StringBuilder();
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e) // Search
+        {
+
+            SpaceIndexes.Clear();
+
+            Flag = myString.ToString();
+            Asciibytes = Encoding.ASCII.GetBytes(textBox1.Text);
+            Asciibytes2 = Encoding.ASCII.GetBytes(Flag);
+
+            for (int i = 0; i < Asciibytes2.Length; i++)
+            {
+                if (Asciibytes2[i] == 32) SpaceIndexes.Add(i);
+                if (i == Asciibytes2.Length - 1) SpaceIndexes.Add(Asciibytes2.Length);
+            }
+
+            int sum = 0;
+            for (int i = 0; i < Asciibytes.Length; i++)
+            {
+                sum += (Asciibytes[i] * (i + 1));
+            }
+
+            if (SpaceIndexes.Count > 1)
+            {
+                for (int i = 1; i < SpaceIndexes.Count; i++)
+                {
+                    if (sum == Search(SpaceIndexes[i - 1], SpaceIndexes[i]))
+                    {
+                        richTextBox1.Select(SpaceIndexes[i - 1], SpaceIndexes[i] - SpaceIndexes[i - 1]);
+                        richTextBox1.SelectionColor = Color.Blue;
+                        richTextBox1.SelectionBackColor = Color.Wheat;
+                    }
+                }
+            }
+        }
+
+        public int Search(int startIndex, int EndIndex)
+        {
+            int sum = 0; int k = 0;
+            for (int i = startIndex; i < EndIndex; i++)
+            {
+                sum += (Asciibytes2[i] * (k + 1));
+                k++;
+            }
+            return sum;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            myString.Clear();
+            myString.Append(richTextBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = richTextBox1.Text.Replace(textBox1.Text, textBox2.Text);
+        }
+
+        public static string ReverseString(string word)
+        {
+            char[] arr = word.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+       
+    }
+}
